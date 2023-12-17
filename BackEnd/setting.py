914428140e201e03bc71dd6698594363a08bdbb1
Nguyễn Duy Hòa <hoa.nguyen2020@hcmut.edu.vn>
2023-12-17 21:48:16 +0700

    Remove unnecessary files and code

---
+++ BackEnd/setting.py
@@ -0,0 +1,3 @@
+# MongoDB attributes
+mongodb_uri = 'mongodb+srv://tanio:<Thanh#253>@tanio.wjgr0ct.mongodb.net/?retryWrites=true&w=majority'
+port = 8000  
--- setting.py
+++
@@ -1,3 +0,0 @@
-# MongoDB attributes
-mongodb_uri = 'mongodb+srv://tanio:<Thanh#253>@tanio.wjgr0ct.mongodb.net/?retryWrites=true&w=majority'
-port = 8000  
