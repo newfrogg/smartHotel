b5e7d705780a9ca6a6de0e8ac6536e2f49eabecd
HnKnA <79851373+HnKnA@users.noreply.github.com>
2023-12-17 22:06:32 +0700

    Update mongo uri

--- BackEnd/setting.py
+++ BackEnd/setting.py
@@ -1,3 +1,3 @@
 # MongoDB attributes
-mongodb_uri = 'mongodb+srv://tanio:<Thanh#253>@tanio.wjgr0ct.mongodb.net/?retryWrites=true&w=majority'
+mongodb_uri = 'mongodb+srv://embedded:kinhtehocdaicuong@tuan756.ixx9tno.mongodb.net/?retryWrites=true&w=majority'
 port = 8000  
